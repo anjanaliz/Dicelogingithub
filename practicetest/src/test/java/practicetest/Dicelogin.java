d7838f6e912e55c5f695a1b046edec81708e888a
AnjanaLizbethJose <137315928+AnjanaLizbethJose@users.noreply.github.com>
2024-05-16 22:32:03 -0500

    First commit

---
+++ practicetest/src/test/java/practicetest/Dicelogin.java
@@ -0,0 +1,48 @@
+package practicetest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Dicelogin {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void methodsetup()
+	{
+	driver = new FirefoxDriver();
+	}
+	
+	@AfterTest
+	public void methodTeardown()
+	{
+		driver.quit();
+	}
+	
+	
+	
+	@Test
+	public void loginsuccess() {
+        driver.get("https://www.dice.com/dashboard/login");
+		System.out.println("test loginsuccess");
+		System.out.println("First edit");
+	}
+	@Test
+	public void loginfailure() {
+		driver.get("https://www.dice.com/dashboard/login");
+		System.out.println("test loginfailure");
+
+	}
+
+	private void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds * 10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
